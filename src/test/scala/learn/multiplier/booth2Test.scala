ab647e900aa72e27964910f9e041113bb87f921b
DavidZyy <1929772352@qq.com>
2023-10-29 21:25:51 +0800

    booth 2 bit algo past

--- src/test/scala/learn/multiplier/booth2Test.scala
+++ src/test/scala/learn/multiplier/booth2Test.scala
@@ -7,15 +7,15 @@ import org.scalatest.flatspec.AnyFlatSpec
 import learn.adder.Constants._
 import java.util.Random
 
-class booth1Test extends AnyFlatSpec with ChiselScalatestTester {
-    val bitWidth = 4
-    "booth1" should "pass" in {
-        test(new booth1bit(bitWidth)) { dut =>
-            // for(i <- 0 until 100) {
-                // val A = BigInt(bitWidth, new Random) - (BigInt(1) << (bitWidth - 1))
-                // val B = BigInt(bitWidth, new Random) - (BigInt(1) << (bitWidth - 1))
-                val A = BigInt(-2) //-2 
-                val B = BigInt(3)
+class booth2Test extends AnyFlatSpec with ChiselScalatestTester {
+    val bitWidth = 64
+    "booth2" should "pass" in {
+        test(new booth2bit(bitWidth)) { dut =>
+            for(i <- 0 until 1000) {
+                val A = BigInt(bitWidth, new Random) - (BigInt(1) << (bitWidth - 1))
+                val B = BigInt(bitWidth, new Random) - (BigInt(1) << (bitWidth - 1))
+                // val A = BigInt(-1) //-2 
+                // val B = BigInt(-7)
                 val product = A*B // should be the complete of -6
                 // val product = BigInt(-6)
 
@@ -30,11 +30,11 @@ class booth1Test extends AnyFlatSpec with ChiselScalatestTester {
                 dut.clock.step(1)
                 dut.reset.poke(false.B) // Deassert reset
 
-                dut.clock.step(bitWidth)
+                dut.clock.step(bitWidth/2)
 
                 dut.io.product.expect(product.S)
 
-            // }
+            }
         }
     }
 }
